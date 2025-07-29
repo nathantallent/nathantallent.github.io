ec5f5c1f4ed2203cf3a7d06e1c2bcfcb12e3d0aa
Tallent, Nathan R <nathan.tallent@pnnl.gov>
2025-07-29 09:24:15 -0700

    first commit

---
+++ style/footer.js
@@ -0,0 +1,12 @@
+// Footer
+document.write('\
+  <div> \
+  <hr /> \
+  <p style="margin: 0px;" > \
+    <a href="http://validator.w3.org/check?uri=https%3A%2F%2Fhpc.pnnl.gov%2Fpeople%2Ftallent%2F"> \
+      <img src="http://www.w3.org/Icons/valid-xhtml10" alt="" height="15" /></a> \
+    <a href="http://jigsaw.w3.org/css-validator/validator?uri=https%3A%2F%2Fhpc.pnnl.gov%2Fpeople%2Ftallent%2F&profile=css3svg&usermedium=all&warning=1"> \
+      <img src="https://jigsaw.w3.org/css-validator/images/vcss-blue" alt="" height="15" /></a> \
+  </p> \
+  </div> \
+');
---
+++ style/navigation-map.js
@@ -0,0 +1,10 @@
+// We use JavaScript as a way to easily include this navigation map
+// in several HTML pages.
+
+// Menu items
+document.write('<div class="map">');
+document.write('  <p><a href="index.html">Home</a>');
+document.write('  <p><a href="research.html">Research</a>');
+//document.write('  <p><a href="black-holes.html">Black holes</a>');
+document.write('</div>');
+
