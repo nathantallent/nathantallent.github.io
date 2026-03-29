d536fde122778321f50098e62188d90ecc1260a1
Tallent, Nathan R <nathan.tallent@pnnl.gov>
2026-03-29 15:23:59 -0700

    Updates to profile

--- style/footer.js
+++ style/footer.js
@@ -3,8 +3,7 @@ document.write('\
   <div> \
   <hr /> \
   <p style="margin: 0px;" > \
-    <a href="https://validator.w3.org/check?uri=https%3A%2F%2Fhpc.pnnl.gov%2Fpeople%2Ftallent%2F"> \
-      <img src="https://www.w3.org/Icons/valid-xhtml10" alt="" height="15" /></a> \
+    <a href="https://validator.w3.org/check/referer">[validate]</a> \
     <a href="https://jigsaw.w3.org/css-validator/validator?uri=https%3A%2F%2Fhpc.pnnl.gov%2Fpeople%2Ftallent%2F&profile=css3svg&usermedium=all&warning=1"> \
       <img src="https://jigsaw.w3.org/css-validator/images/vcss-blue" alt="" height="15" /></a> \
   </p> \
