3f80d858df3ade34b3a7fc4fe24b281b68ef3fa6
Tallent, Nathan R <nathan.tallent@pnnl.gov>
2026-03-29 17:09:32 -0700

    Updates to profile

--- style/footer.js
+++ style/footer.js
@@ -2,9 +2,9 @@
 document.write('\
   <div> \
   <hr /> \
-  <p style="margin: 0px;" > validate \
-    <a href="https://validator.w3.org/check/referer">[html]</a> \
-    <a href="https://jigsaw.w3.org/css-validator/validator?">[css]</a> \
+  <p style="margin: 0px;" > \
+    <a href="http://validator.w3.org/check/referer"><img src="http://www.w3.org/Icons/valid-xhtml10" alt="" height="15"></a> \
+    <a href="https://jigsaw.w3.org/css-validator/validator?"><img src="https://jigsaw.w3.org/css-validator/images/vcss-blue" alt="" height="15"></a> \
   </p> \
   </div> \
 ');
